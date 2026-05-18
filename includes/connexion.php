61eaf8a9fc26e45c28c9d4ba9c11fb68f7b3e1ae
Ahmadousock10-cmd <ahmadou.sock10@gmail.com>
2026-05-19 00:01:47 +0200

    Update connexion.php

--- includes/connexion.php
+++ includes/connexion.php
@@ -1,14 +1,1 @@
-<?php
-define('DB_HOST', 'sql301.infinityfree.com');
-define('DB_USER', 'if0_41820138');
-define('DB_PASS', '0qGQ2D3WFA8');
-define('DB_NAME', 'if0_41820138_agrisen');
 
-$connexion = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-
-if (!$connexion) {
-die("Erreur de connexion : " . mysqli_connect_error());
-}
-
-mysqli_set_charset($connexion, 'utf8');
-?>
