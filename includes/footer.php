9e5f6b5ef6f4f05af6e0b1472d963f2b527c7081
Ahmadousock10-cmd <ahmadou.sock@unchk.edu.sn>
2026-06-09 23:12:06 +0200

    Remove JS scripts from footer.php
    Remove Bootstrap and custom JS scripts from footer.

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,1 @@
-<!-- Bootstrap JS -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
-<!-- Notre JS personnalisé -->
-<script src="/assets/js/main.js"></script>
-</body>
-</html>
