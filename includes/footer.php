56b35ebe37b8b9fbd81c9de97947bf7f4c3541fb
Ahmadousock10-cmd <ahmadou.sock10@gmail.com>
2026-06-09 23:00:16 +0200

    Ajouter Bootstrap et des scripts JS personnalisés au pied de page

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,7 @@
+<!-- Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
+<!-- Notre JS personnalisé -->
+<script src="/assets/js/main.js"></script>
+</body>
+</html>
